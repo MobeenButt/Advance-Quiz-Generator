34a72eb02febf0a9ae46847c5a2e4bd268b47636
mtalha1501 <mtalha1501@gmail.com>
2024-12-15 00:40:45 +0500

    commiting

--- AdvanceQuizApp/AdvanceQuizApp/obj/Debug/net8.0-windows/AdvanceQuizApp.AssemblyInfo.cs
+++ AdvanceQuizApp/AdvanceQuizApp/obj/Debug/net8.0-windows/AdvanceQuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245f7f63f5ba8c595b3911541f0c89b8bb02dc35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9bc90917c34fb3a1c516129bcdb1e5cb3fc9c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
