9f2b7a56cbfa6352c162d04b5a63b659e4f0a459
anas faiz <anasfaizsahi6@gmail.com>
2024-12-15 23:50:33 +0500

    added

--- AdvanceQuizApp/AdvanceQuizApp/obj/Debug/net8.0-windows/AdvanceQuizApp.AssemblyInfo.cs
+++ AdvanceQuizApp/AdvanceQuizApp/obj/Debug/net8.0-windows/AdvanceQuizApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aadfe81af674865e2fad6b2f9ad90b087199891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de34b8db581db68ab9e02bb95a64ac81e7cb50a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvanceQuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
